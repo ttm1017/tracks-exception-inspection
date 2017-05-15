c21cc4d6b777f873d10f49dcf51101f26c930216
tanming.ttm <tanming.ttm@alibaba-inc.com>
2017-05-15 16:27:16 +0800

    add websocket

--- server.js
+++ server.js
@@ -6,12 +6,26 @@ const Koa = require('koa')
     , logger = require('koa-logger')
     , onerror = require('koa-onerror')
     , bodyParser = require('koa-bodyparser')
-    , mount = require('koa-mount');
+    , mount = require('koa-mount')
+    , WebSocket = require('ws');
 
 const index = require('./server/routes/index');
 const app = new Koa();
 // error handler
 onerror(app);
+//build websocket
+const wss = new WebSocket.Server({ port: 8001 });
+
+wss.on('connection', function connection(ws) {
+    ws.on('message', function incoming(message) {
+        console.log('received: %s', message);
+        if (message.status == 'overLead') {
+
+        }
+    });
+
+    ws.send(JSON.stringify({a:1}));
+});
 
 // global middlewares
 
